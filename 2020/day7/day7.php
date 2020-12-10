1c0a614236b8eb2aad9f41dffeb859686c042386
marberrym <marberrym@gmail.com>
2020-12-09 20:33:52 -0500

    Working on solving day 7 with php... getting closer but no cigar.

--- 2020/day7/day7.php
+++ 2020/day7/day7.php
@@ -11,7 +11,53 @@
  * @link     http://localhost/
  */
 
+$keys = array();
 $data = prepData();
+$count = 0;
+
+class Bag {
+    public function __construct($bag_name, $contained_bags)
+    {
+        $this->bag_name = $bag_name;
+        $this->contained_bags = $contained_bags;
+    }
+
+    public function getBagName()
+    {
+        return $this->bag_name;
+    }
+
+    public function getContainedBags($data)
+    {
+        return array_filter($data, function ($obj) {
+            if (in_array($obj->bag_name, $this->contained_bags)) {
+                return true;
+            }
+        }
+        );
+        // return $this->contained_bags;
+    }
+
+    public function hasGoldBag()
+    {
+        $searchTerm = "shiny gold bags";
+        if (in_array($searchTerm, $this->contained_bags)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function containsNoBags()
+    {
+        $searchTerm = "no other bags";
+        if (in_array($searchTerm, $this->contained_bags)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
 
  /**
   * Prepares dataset to be used
@@ -20,8 +66,7 @@ $data = prepData();
   */
 function prepData()
 {
-
-    $response = (object)
+    $response = array();
     $file="input.txt";
     $fopen = fopen($file, "read");
     $fread = fread($fopen, filesize($file));
@@ -38,12 +83,10 @@ function prepData()
 
         $rules = formatBags($inside);
 
-        $response->$key = $rules;
-        // print_r("{$key}\n");
-        // print_r("{$inside}\n");
-    }
+        $addedBag = new Bag($key, $rules);
 
-    print_r($response);
+        array_push($response, $addedBag);
+    }
 
     return $response;
 }
@@ -90,7 +133,7 @@ function formatBag($string)
         $formattedBag = "{$formattedBag}s";
     }
 
-    return $formattedBag;
+    return trim($formattedBag);
 }
 
 
@@ -103,15 +146,34 @@ function formatBag($string)
  */
 function checkBags($data)
 {
-    print_r(gettype($data));
-    for ($x=0; $x<count($data); $x++) {
-        print_r($x);
-        print_r($data[$x]);
+    // print_r($data);
+    $count = 0;
+
+
+    foreach ($data as $bag) {
+        if (checkForGoldBag($bag, $data)) {
+            $count++;
+        }
     }
-    // foreach ($data as $bag) {
-    //     print_r("Bag entry...");
-    //     print_r($bag);
-    // }
+
+    echo $count;
+
 }
 
-checkBags($data);
+function checkForGoldBag($bag, $data)
+{
+    if ($bag->hasGoldBag()) {
+        return true;
+    } else if ($bag->containsNoBags()) {
+        return false;
+    } else {
+        foreach ($bag->getContainedBags($data) as $bagToCheck) {
+            if (checkForGoldBag($bagToCheck, $data)) {
+                return true;
+            };
+        }
+    }
+}
+
+checkBags($data, $count);
+echo($count);
