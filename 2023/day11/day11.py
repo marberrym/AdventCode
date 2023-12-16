c4e74eb6d1d0c0895722e08ca62180fee4dd6c3f
Matthew Marberry <marberrym@gmail.com>
2023-12-15 19:31:52 -0500

    Updates.

--- 2023/day11/day11.py
+++ 2023/day11/day11.py
@@ -1,5 +1,5 @@
 f = open('input.txt', 'r');
-input = f.read().splitlines();
+puzzle_input = f.read().splitlines();
 tf = open('testCase.txt', 'r');
 test = tf.read().splitlines();
 
@@ -7,14 +7,18 @@ test = tf.read().splitlines();
 def check_column(galaxy_grid, col_idx):
     should_expand = True
     for row in galaxy_grid:
-        if row[col_idx] == '.': continue
-        else: should_expand = False
+        if row[col_idx] == '.':
+            continue
+        else:
+            should_expand = False
     return should_expand
+
 
 def generate_row(num):
     row = []
     for _ in range(num): row.append('.')
     return row
+
 
 def expand_galaxy(galaxy_grid):
     grid = galaxy_grid
@@ -23,12 +27,12 @@ def expand_galaxy(galaxy_grid):
 
     for row_idx, row in enumerate(grid):
         if all([x == '.' for x in row]): rows_to_insert.append(row_idx)
-    
+
     for col_idx, col in enumerate(grid[0]):
         if col == '.':
             if check_column(grid, col_idx):
                 cols_to_insert.append(col_idx)
-            
+
     for idx in rows_to_insert:
         grid.insert(idx, generate_row(len(grid[0])))
 
@@ -38,6 +42,7 @@ def expand_galaxy(galaxy_grid):
 
     return grid
 
+
 def find_all_galaxy_coords(grid):
     coords = []
     for row_idx, row in enumerate(grid):
@@ -46,60 +51,96 @@ def find_all_galaxy_coords(grid):
                 coords.append([row_idx, col_idx])
     return coords
 
-def find_path(start, end):
-    current = start
-    steps = 0
 
-    while current != end:
-        if current[0] < end[0]:
-            current[0] += 1
-            steps += 1
-        if current[0] > end[0]:
-            current[0] -= 1
-            steps += 1
-        if current[1] < end[1]:
-            current[1] += 1
-            steps += 1
-        if current[1] > end[1]:
-            current[1] += 1
-            steps += 1
-    
+def check_point(start, end):
+
+    if (abs(end[0] - start[0]) == 1 and abs(end[1] - start[1]) == 0) or (end[0] == start[0] and end[1] == start[1
+
+        or end[0] == start[0]) and (abs(end[1] - start[1]) == 1 or end[1] == start[1]):
+    if (abs(end[0] - start[0]) == 1 or end[0] == start[0]) and (abs(end[1] - start[1]) == 1 or end[1] == start[1]):
+        return True
+    else:
+        return False
+
+
+def find_path(start, end):
+    current = start.copy()
+    steps = 0
+    last_step_was_vertical = True
+
+    while not check_point(current, end):
+        print("CURRENT", current)
+        print(check_point(current, end))
+
+        vertical_diff = abs(current[0] - end[0])
+        horizontal_diff = abs(current[1] - end[1])
+        if vertical_diff > 1:
+            if current[0] < end[0]:
+                current[0] += 1
+                steps += 1
+            if current[0] > end[0]:
+                current[0] -= 1
+                steps += 1
+            last_step_was_vertical = False
+        elif horizontal_diff > 1:
+            if current[1] < end[1]:
+                current[1] += 1
+                steps += 1
+            if current[1] > end[1]:
+                current[1] -= 1
+                steps += 1
+            last_step_was_vertical = True
+
     return steps
 
 
-def find_paths(coords):
-    paths = {}
+def get_coords_without_start(coords, start, completed_coords):
+    return list(filter(lambda x: x != start and x not in completed_coords, coords))
+
+
+def find_step_count_for_coord(start_coord, goal_coords):
+    steps = 0
+    for goal_coord in goal_coords:
+        step_count = find_path(start_coord, goal_coord)
+        print("Step count is...", step_count)
+        steps += step_count
+    return steps
+
+
+def generate_paths(coords):
+    paths = []
     completed_coords = []
-    coords_clone = coords
     for coord in coords:
-        print("START WITH...", coord)
-        coords_without_start_point = filter(lambda x: x != coord and x not in completed_coords, coords_clone)
-        print("COORDS WITHOUT START...", list(coords_without_start_point))
-        for goal_coord in list(coords_without_start_point):
-            print(goal_coord)
-            steps = find_path(coord, goal_coord)
-            key = f"{coord[0]},{coord[1]}-{goal_coord[0]},{goal_coord[1]}"
-            print(key)
-            paths[key] = steps
-            
+        paths.append({
+            "start": coord,
+            "goals": get_coords_without_start(coords, coord, completed_coords)
+        })
         completed_coords.append(coord)
 
-    print(paths)
+    return paths
 
-    
 
-def main(input):
-    galaxy_grid = [list(x) for x in input]
+def find_paths(coords):
+    paths = generate_paths(coords)
+    total_steps = 0
+
+    for coord in paths:
+        total_steps += find_step_count_for_coord(coord["start"], coord["goals"])
+
+    print(total_steps)
+
+
+def main(data):
+    galaxy_grid = [list(x) for x in data]
 
     expanded_galaxy_grid = expand_galaxy(galaxy_grid)
     galaxy_coords = find_all_galaxy_coords(expanded_galaxy_grid)
-
-    
 
     paths = find_paths(galaxy_coords)
     for row in expanded_galaxy_grid:
         print(row)
 
     print(galaxy_coords)
+
 
 main(test)
--- 2023/day7/day7.py
+++ 2023/day7/day7.py
@@ -1,8 +1,106 @@
 f = open('input.txt', 'r');
-input = f.read().splitlines();
+puzzle_input = f.read().splitlines();
 tf = open('testCase.txt', 'r');
 test = tf.read().splitlines();
+from collections import Counter
 
-def main(input):
+FIVE_OF_A_KIND = "Five of a kind"
+FOUR_OF_A_KIND = "Four of a kind"
+FULL_HOUSE = "Full house"
+THREE_OF_A_KIND = "Three of a kind"
+TWO_PAIR = "Two pair"
+ONE_PAIR = "One pair"
+HIGH_CARD = "High card"
 
-main(test)
+card_values = {
+    "A": 14,
+    "K": 13,
+    "Q": 12,
+    "J": 11,
+    "T": 10,
+    "9": 9,
+    "8": 8,
+    "7": 7,
+    "6": 6,
+    "5": 5,
+    "4": 4,
+    "3": 3,
+    "2": 2
+}
+
+def remove_card(hand, card):
+    return [c for c in hand if c != card]
+
+
+def find_pair(hand):
+    for idx, card in enumerate(hand):
+        if hand.count(card) == 2:
+            return True
+        elif idx == len(hand) - 1:
+            return False
+
+
+def analyze_hand(hand):
+    cards = list(hand)
+    counts = sorted(Counter(cards).values(), reverse=True)
+
+    if counts[0] == 5:
+        return {"hand_type": FIVE_OF_A_KIND}
+    if counts[0] == 4:
+        return {"hand_type": FOUR_OF_A_KIND}
+    if counts[0] == 3 and counts[1] == 2:
+        return {"hand_type": FULL_HOUSE}
+    if counts[0] == 3:
+        return {"hand_type": THREE_OF_A_KIND}
+    if counts[0] == 2 and counts[1] == 2:
+        return {"hand_type": TWO_PAIR}
+    if counts[0] == 2:
+        return {"hand_type": ONE_PAIR}
+    return {"hand_type": HIGH_CARD}
+
+def sort_hands(hands):
+    return sorted(hands, key=lambda hand: [card_values[c] for c in hand], reverse=True)
+
+
+def main(puzzle_input):
+    hands = {
+        FIVE_OF_A_KIND: [],
+        FOUR_OF_A_KIND: [],
+        FULL_HOUSE: [],
+        THREE_OF_A_KIND: [],
+        TWO_PAIR: [],
+        ONE_PAIR: [],
+        HIGH_CARD: []
+    }
+
+    ranked_hands = []
+    total_winnings = 0
+
+    for line in puzzle_input:
+        hand = line.split()[0]
+        analyzed_hand = analyze_hand(hand)
+        hand_type = analyzed_hand['hand_type']
+
+        hands[hand_type].append(hand)
+
+    for hand_type in hands.keys():
+        hands[hand_type] = sort_hands(hands[hand_type])
+        ranked_hands.extend(hands[hand_type])
+        print(hand_type, hands[hand_type], "\n")
+
+    for line in puzzle_input:
+        hand, bid = line.split()
+        ranking = list(reversed(ranked_hands)).index(hand) + 1
+        winnings = ranking * int(bid)
+        total_winnings += winnings
+        print("Hand: ", hand, " Bid: ", bid, " Winnings: ", winnings, " Ranking... ", ranking)
+
+    for idx, rank in enumerate(list(reversed(ranked_hands))):
+        print (idx + 1, rank)
+
+
+    print("Total winnings: ", total_winnings)
+
+#  250950896
+main(puzzle_input)
+
